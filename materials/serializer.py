55d280a354b4975fb5ee74614791b48763922c98
LudaKend <Ludakend@gmail.com>
2024-02-14 20:40:51 +0300

    Задание 1.количество уроков в сериалайзере для модели Course с помощью метода SerializerMethodField.

--- materials/serializer.py
+++ materials/serializer.py
@@ -1,12 +1,23 @@
 from rest_framework import serializers
+from rest_framework.serializers import SerializerMethodField
 from materials.models import Course, Lesson
 
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = '__all__'
+
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+
+class CourseSerializer(serializers.ModelSerializer):
+
+    quantity_lesson = SerializerMethodField()
+
+    def get_quantity_lesson(self, course):
+        list_lesson = Lesson.objects.filter(course=course)
+        return len(list_lesson)
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
