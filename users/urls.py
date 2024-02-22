e9b93c30aa7e0b591e9f3471e4ad309b3955336b
LudaKend <Ludakend@gmail.com>
2024-02-22 21:52:26 +0300

    Задание 1.CRUD для пользователей, JWT для авторизации, все контроллеры в приложении users, кроме регистрации пользователя, закрыла доступ для неавторизованных пользователей

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,15 @@
 from users.apps import UsersConfig
-from users.views import PaymentListAPIView
+from users.views import PaymentListAPIView, UserCreateAPIView, MyTokenObtainPairView, UserRetrieveAPIView
 from django.urls import path
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
 
     path('payment_list_view/', PaymentListAPIView.as_view(), name='payment_list'),
+    path('token/', MyTokenObtainPairView.as_view(), name='get_token'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
+    path('register/', UserCreateAPIView.as_view(), name='register_users'),
+    path('view/<int:pk>/', UserRetrieveAPIView.as_view(), name='user_view'),
     ]
