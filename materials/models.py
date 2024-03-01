452c11fff546cbbb62f9745ec4709ce99b9bedc5
LudaKend <Ludakend@gmail.com>
2024-03-01 09:04:48 +0300

    Задание 2. Модель Subscription

--- materials/models.py
+++ materials/models.py
@@ -36,3 +36,6 @@ class Lesson(models.Model):
         verbose_name = 'Урок'
         verbose_name_plural = 'Уроки'
 
+class Subscription(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Пользователь')
+    course = models.ForeignKey('Course', on_delete=models.CASCADE, verbose_name='Курс')
